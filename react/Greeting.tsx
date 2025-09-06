e137916cdf8f4b10ae897703ec08b4747f50e8d7
MarcosMatozo <marcoshenriquesp@hotmail.com>
2025-09-06 09:55:15 -0300

    init

---
+++ react/Greeting.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {
+  name: string
+}
+
+function Greeting({ name }: Props) {
+  return <div>Heyssssssssssssssssssssssssssssssssssssss, {name}</div>
+}
+
+export default Greeting
