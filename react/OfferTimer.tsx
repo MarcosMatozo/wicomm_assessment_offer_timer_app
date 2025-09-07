4365a334d71ea7c6ca0758b0e0c8b386079a3a4a
MarcosMatozo <marcoshenriquesp@hotmail.com>
2025-09-07 15:07:35 -0300

    App Completed

---
+++ react/OfferTimer.tsx
@@ -0,0 +1,81 @@
+import React, { useEffect, useState } from 'react'
+import { useCssHandles } from 'vtex.css-handles'
+import './css/style.css'
+
+const CSS_HANDLES = ['container', 'inner', 'title', 'titleDesc', 'timer', 'left', 'block', 'divider', 'dateTitle'] as const
+
+// Define a data-alvo da promoção (formato: YYYY-MM-DDTHH:MM:SS)
+const TARGET_DATE = new Date('2025-09-12T23:59:59')
+
+function OfferTimer() {
+  const { handles } = useCssHandles(CSS_HANDLES)
+  const [timeLeft, setTimeLeft] = useState(getTimeRemaining(TARGET_DATE))
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTimeLeft(getTimeRemaining(TARGET_DATE))
+    }, 1000)
+
+    return () => clearInterval(interval)
+  }, [])
+
+  return (
+    <div className={handles.container}>
+      <div className={handles.inner}>
+
+        {/* Left side */}
+        <div className={handles.left}>
+          <div className={handles.title}>OFERTAS LIMITADAS</div>
+          <div className={handles.titleDesc}>Produtos com preços imperdíveis</div>
+        </div>
+
+        {/* Right side */}
+        <div className={handles.timer}>
+          <div className={`${handles.block}`} >
+            <p>{`${timeLeft.days}`}</p>
+            <p className={handles.dateTitle}>Dias</p>
+          </div>
+          <div className={handles.divider}>:</div>
+          <div className={`${handles.block}`} >
+            <p>{`${timeLeft.hours}`}</p>
+            <p className={handles.dateTitle}>Horas</p>
+          </div>
+          <div className={handles.divider}>:</div>
+          <div className={`${handles.block}`} >
+            <p>{`${timeLeft.minutes}`}</p>
+            <p className={handles.dateTitle}>Minutos</p>
+          </div>
+          <div className={handles.divider}>:</div>
+          <div className={`${handles.block}`} >
+            <p>{`${timeLeft.seconds}`}</p>
+            <p className={handles.dateTitle}>Segundos</p>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  )
+}
+
+function getTimeRemaining(targetDate: Date) {
+  const total = targetDate.getTime() - new Date().getTime()
+
+  const seconds = Math.floor((total / 1000) % 60)
+  const minutes = Math.floor((total / 1000 / 60) % 60)
+  const hours = Math.floor((total / (1000 * 60 * 60)) % 24)
+  const days = Math.floor(total / (1000 * 60 * 60 * 24))
+
+  return {
+    total,
+    days: pad(days),
+    hours: pad(hours),
+    minutes: pad(minutes),
+    seconds: pad(seconds),
+  }
+}
+
+function pad(num: number) {
+  return num < 10 ? `0${num}` : `${num}`
+}
+
+export default OfferTimer
